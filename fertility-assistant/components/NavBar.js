0db14f10bcc9dfdb335ecb6b1a5c53e35ab97d98
Shraddha <shradp21@gmail.com>
2025-11-12 18:57:52 -0500

    NavBar fix

--- fertility-assistant/components/NavBar.js
+++ fertility-assistant/components/NavBar.js
@@ -1,11 +1,16 @@
 import Link from 'next/link';
+import styles from '../styles/NavBar.module.css';
 export default function NavBar() {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/timeline">Journey Timeline</Link>
-      <Link href="/coverage">Coverage Decoder</Link>
-      <Link href="/cost-simulation">Cost Simulator</Link>
+    <nav className={styles.nav}>
+      <div className={styles.logo}>
+        <Link href="/">Home</Link>
+      </div>
+      <ul className={styles.navLinks}>
+        <li><Link href="/timeline">Journey Timeline</Link></li>
+        <li><Link href="/coverage">Coverage Decoder</Link></li>
+        <li><Link href="/cost-simulation">Cost Simulator</Link></li>
+      </ul>
     </nav>
   );
 }
